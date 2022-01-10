87fa30ea23d2e9836697faa4a38ed46a3fbc1466
Beyn <erdener.unal@beyn.com.tr>
2022-01-10 09:24:45 +0300

    metarial-ui added.

---
+++ src/styles/collections.js
@@ -0,0 +1,18 @@
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+export  {
+  Container,
+  Grid,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+};
