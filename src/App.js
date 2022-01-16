8803e62ff7baa29e05cc5f0a86f1c6658b056990
Beyn <erdener.unal@beyn.com.tr>
2022-01-16 21:30:44 +0300

    metarial-ui deleted, tailwind installed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import * as Mui from "./styles/collections";
-import { makeStyles } from "@mui/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import {
   BrowserRouter as Router,
   Navigate,
@@ -8,42 +5,15 @@ import {
   Routes,
 } from "react-router-dom";
 import PostList from "./components/PostList";
+import Navbar from "./components/Navbar";
+import "./index.css";
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: "2rem",
-  },
-});
 
 const App = () => {
-  const classes = useStyles();
   return (
     <>
-      <CssBaseline>
-        <Mui.Container maxWidth="lg">
-          <Mui.AppBar position="static" color="inherit" elevation={0}>
-            <Mui.Toolbar>
-              <Mui.IconButton edge="start" />
-              <Mui.Typography variant="h6" color="secondary">
-                <a href="http://localhost:5000/posts">filmAjandam</a>
-              </Mui.Typography>
-            </Mui.Toolbar>
-          </Mui.AppBar>
-
-          <Mui.Grid container>
-            <Router>
-              <Routes>
-                <Route exact path="/posts" component={PostList}></Route>
-
-                <Route path="*" element={<Navigate to="/" />}></Route>
-              </Routes>
-            </Router>
-          </Mui.Grid>
-        </Mui.Container>
-      </CssBaseline>
+      <Navbar/>
+      
     </>
   );
 };
